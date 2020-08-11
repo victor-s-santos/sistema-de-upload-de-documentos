eeaa8ac55342ec5def29b3ee9f3415348d3235b9
victor-s-santos <victor.s.silva@unesp.br>
2020-08-11 16:26:48 -0300

    retorna os erros de inscrição no template

--- register/views.py
+++ register/views.py
@@ -1,6 +1,7 @@
 from django.core import mail
 from django.contrib import messages
 from django.shortcuts import render, redirect
+from django.template.loader import render_to_string
 from .forms import RegisterForm
 
 corpo_email = 'Teste'
@@ -8,28 +9,22 @@ corpo_email = 'Teste'
 def register(request):
     if request.method == "POST":
         form = RegisterForm(request.POST)
-        if form.is_valid():
+
+        if form.is_valid(): 
             form.save()
             messages.success(request, 'Usuário cadastrado com sucesso! Por favor, faça o Login para poder utilizar o sistema.')
+            body = render_to_string('register/corpo_email_inscricao.txt', 
+                                    form.cleaned_data)
             mail.send_email('Confirmação de inscrição',
-                            message,
+                            body,
                             'contato@jurisfai.com.br',
-                            ['contato@jurisfai.com.br', 'victorsantos.py@gmail.com'])
-        return redirect('register')
+                            ['contato@jurisfai.com.br', 'victorsantos.py@gmail.com', form.cleaned_data['email']])
+        
+            return redirect('register')
+        else:
+            return(render(request, 'register/register.html',
+                            {'form': form}))
     else:
         form = RegisterForm()
     return render(request, 'register/register.html', {'form': form})
 
-message = 
-"""
-Olá, o seu usuário já se encontra disponível!
-
-Agradecemos a sua inscrição, utilizando o nome de usuário cadastrado já é possível submeter documentos à análise.
-Mais uma vez, muito obrigado pela confiança em nosso produto.
-
-Nome de usuário:
-Email:
-
-Atenciosamente,
-Equipe JurisfAI
-"""
