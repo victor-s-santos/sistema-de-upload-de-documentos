09f4d789df1420b8f52b71e2e5cce1753897b65c
victor-s-santos <victor.s.silva@unesp.br>
2020-08-11 16:02:01 -0300

    construindo o corpo do email

--- register/views.py
+++ register/views.py
@@ -1,3 +1,4 @@
+from django.core import mail
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
@@ -9,9 +10,26 @@ def register(request):
         form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
-        messages.success(request, 'Usuário cadastrado com sucesso! Por favor, faça o Login para poder utilizar o sistema.')
+            messages.success(request, 'Usuário cadastrado com sucesso! Por favor, faça o Login para poder utilizar o sistema.')
+            mail.send_email('Confirmação de inscrição',
+                            message,
+                            'contato@jurisfai.com.br',
+                            ['contato@jurisfai.com.br', 'victorsantos.py@gmail.com'])
         return redirect('register')
     else:
         form = RegisterForm()
     return render(request, 'register/register.html', {'form': form})
-# Create your views here.
+
+message = 
+"""
+Olá, o seu usuário já se encontra disponível!
+
+Agradecemos a sua inscrição, utilizando o nome de usuário cadastrado já é possível submeter documentos à análise.
+Mais uma vez, muito obrigado pela confiança em nosso produto.
+
+Nome de usuário:
+Email:
+
+Atenciosamente,
+Equipe JurisfAI
+"""
