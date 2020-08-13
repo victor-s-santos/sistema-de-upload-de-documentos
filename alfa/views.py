73b160c130c12634fc5d31ec217a91e2a717d70c
victor-s-santos <victor.s.silva@unesp.br>
2020-08-13 15:03:25 -0300

    é exibido múltiplos formulários, mas ainda não funcionam

--- alfa/views.py
+++ alfa/views.py
@@ -3,22 +3,35 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .forms import DocumentosForm
 from .models import Documentos
+from django.forms import modelformset_factory
 
 corpo_email = 'Aviso automático, novo documento enviado. Um usuário acabou de enviar um documento para análise.'
 
 @login_required(login_url='/login')
 def upload_documento(request):
+	DocumentosFormset = modelformset_factory(Documentos, fields=('nome','documento',), extra=5)
 	if request.method == 'POST':
 		form = DocumentosForm(request.POST, request.FILES)
-		if form.is_valid():
+		formset = DocumentosFormset(request.POST or None, request.FILES or None)
+		if form.is_valid() and formset.is_valid():
 			post = form.save(commit=False)
 			post.user = request.user
 			post.save()
-			return redirect('documentos_lista')
+			for f in formset:
+				try:
+					documento = Documentos(user=post.user,nome = post.nome, doc=f.cleaned_data['documento'])
+					documento.save()
+					return redirect('documentos_lista')
+				except Exception as e:
+					break
+
+		return redirect('documentos_lista')
 	else:
 		form = DocumentosForm()
+		formset = DocumentosFormset(queryset = Documentos.objects.filter(user=request.user))
 	return render(request, 'upload_documento.html', {
-		'form': form
+		'form': form,
+		'formset': formset
 	})
 
 @login_required(login_url='/login')
