2e7bf7aa8d5ae37b286bd26b7ab03cd5f59c3875
victor-s-santos <victor.s.silva@unesp.br>
2020-08-19 14:48:23 -0300

    enviando email através de signals

--- alfa/signals.py
+++ alfa/signals.py
@@ -8,11 +8,10 @@ message = 'O documento enviado ao nosso sistema já se encontra trabalhado. Para
 @receiver(signals.post_save, sender=Documentos)
 def documento_enviado(sender, instance, created, **kwargs):
     if instance.trabalhado == True:
-        print('o documento foi enviado')
-        """mail.send_mail('Obrigado por utilizar nosso sistema!',
+        mail.send_mail('Obrigado por utilizar nosso sistema!',
                                 message,
-                                'victorsantos.py@gmail.com',#de quem é o email
-                                ['victorsantos.py@gmail.com', instance.user.email] #para quem é o email, destinatário
-                                )"""
+                                'victorsantos.py@gmail.com',#remetente
+                                ['victorsantos.py@gmail.com', instance.user.email] #destinatário
+                                )
 
     
